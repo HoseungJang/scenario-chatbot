98a0fc452e1193f2643475e1f74c43429da6e7bb
HoseungJang <hsjang.dev@gmail.com>
2020-05-20 13:52:20 +0900

    ADD: skill entity

---
+++ src/entities/skill.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Chatbot } from "./chatbot";
+import { Block } from "./block";
+
+@Entity()
+export class Skill {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: "varchar",
+        nullable: false
+    })
+    name: string;
+
+    @ManyToOne(type => Chatbot, chatbot => chatbot.skills, {
+        nullable: false,
+        onDelete: "CASCADE"
+    })
+    chatbot: Chatbot;
+
+    @OneToMany(type => Block, block => block.skill, {
+        cascade: true
+    })
+    blocks: Block[];
+}
