1b943ad056df6f0328be70c89beb2d34f09071a2
HoseungJang <hsjang.dev@gmail.com>
2020-05-20 22:42:15 +0900

    ADD: dependencyInjectorLoader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import expressLoader from "./express";
 import typeormLoader from "./typeorm";
+import dependencyInjectorLoader from "./dependencyInjector";
 
 export default ({ app }: { app: Application }) => {
     typeormLoader();
     expressLoader({ app });
+    dependencyInjectorLoader();
 };
