87086abbb9a86c3fae3e741c4ddc8ca94c293cbd
HoseungJang <hsjang.dev@gmail.com>
2020-05-20 22:40:41 +0900

    ADD: chatbot router

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import chatbot from "./routers/chatbot";
+
+export default () => {
+    const router: Router = Router();
+    
+    chatbot({ router });
+    return router;
+}
