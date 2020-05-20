6b05e90b951da824439a1c9d5589ee78ebe30898
HoseungJang <hsjang.dev@gmail.com>
2020-05-20 22:41:41 +0900

    ADD: dependency injector;

---
+++ src/loaders/dependencyInjector.ts
@@ -0,0 +1,9 @@
+import { Container } from "typedi";
+import { Chatbot } from "../entities/chatbot";
+import { Skill } from "../entities/skill";
+
+export default () => {
+    Container.set("chatbotEntity", Chatbot);
+
+    Container.set("skillEntity", Skill);
+}
