ab06768bb0b2b9fca451841bedad97e1b7756594
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 16:35:01 +0900

    ADD: createSkill

--- src/services/chatbot.ts
+++ src/services/chatbot.ts
@@ -1,22 +1,45 @@
 import { Service, Inject } from "typedi";
-import { getRepository } from "typeorm";
+import { getManager } from "typeorm";
 import { IChatbot, IChatbotDTO } from "../interfaces/IChatbot";
+import { ISkill, ISkillDTO } from "../interfaces/ISkill";
 
 @Service()
 export class ChatbotService {
     constructor(
-        @Inject("chatbotEntity") private chatbotEntity: Entities.chatbotEntity
+        @Inject("chatbotEntity") private chatbotEntity: Entities.chatbotEntity,
+        @Inject("skillEntity") private skillEntity: Entities.skillEntity
     ) { }
 
     public async createChatbot({ name, role }: IChatbotDTO): Promise<IChatbot> {
         try {
-            const { insertId: id } = (await getRepository(this.chatbotEntity)
-                .createQueryBuilder()
-                .insert()
-                .values({ name, role })
-                .execute()).raw;
+            const entityManager = getManager();
+            const chatbot = new this.chatbotEntity();
+
+            chatbot.name = name;
+            chatbot.role = role;
+            await entityManager.save(chatbot);
+
+            const { id } = chatbot;
 
             return { id, name, role };
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    public async createSkill({ name, chatbotId }: ISkillDTO): Promise<ISkill> {
+        try {
+            const entityManager = getManager();
+            const chatbot = await entityManager.findOne(this.chatbotEntity, chatbotId);
+            const skill = new this.skillEntity();
+            
+            skill.name = name;
+            skill.chatbot = chatbot;
+            await entityManager.save(skill);
+
+            const { id } = skill;
+
+            return { id, name };
         } catch (err) {
             throw err;
         }
