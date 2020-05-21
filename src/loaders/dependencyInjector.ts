e528ae0e3d1fd60813005f88c22d72b807211b9c
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 19:27:48 +0900

    ADD: blockEntity injector

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,9 +1,12 @@
 import { Container } from "typedi";
 import { Chatbot } from "../entities/chatbot";
 import { Skill } from "../entities/skill";
+import { Block } from "../entities/block";
 
 export default () => {
     Container.set("chatbotEntity", Chatbot);
 
     Container.set("skillEntity", Skill);
+
+    Container.set("blockEntity", Block);
 };
