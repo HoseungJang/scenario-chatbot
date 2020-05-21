ed20c895c4338b5bbf2892fae9e211dc057b3195
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 19:26:22 +0900

    ADD: skill router'

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import chatbot from "./routers/chatbot";
+import skill from "./routers/skill";
 
 export default () => {
     const app: Router = Router();
     
     chatbot({ app });
+    skill({ app });
     
     return app;
 };
