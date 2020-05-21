c5b0f25f8e134bd5b3a6cf90c157a72fd2ba34fe
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 19:28:31 +0900

    ADD: generate default block

--- src/services/chatbot.ts
+++ src/services/chatbot.ts
@@ -7,7 +7,8 @@ import { ISkill, ISkillDTO } from "../interfaces/ISkill";
 export class ChatbotService {
     constructor(
         @Inject("chatbotEntity") private chatbotEntity: Entities.chatbotEntity,
-        @Inject("skillEntity") private skillEntity: Entities.skillEntity
+        @Inject("skillEntity") private skillEntity: Entities.skillEntity,
+        @Inject("blockEntity") private blockEntity: Entities.blockEntity
     ) { }
 
     public async createChatbot({ name, role }: IChatbotDTO): Promise<IChatbot> {
@@ -32,10 +33,15 @@ export class ChatbotService {
             const entityManager = getManager();
             const chatbot = await entityManager.findOne(this.chatbotEntity, chatbotId);
             const skill = new this.skillEntity();
+            const block = new this.blockEntity();
             
             skill.name = name;
             skill.chatbot = chatbot;
             await entityManager.save(skill);
+
+            block.name = "시작";
+            block.skill = skill;
+            await entityManager.save(block);
 
             const { id } = skill;
 
