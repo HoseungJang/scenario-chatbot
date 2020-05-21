739962089b4d2e5d307b0fd299a3e2b356418d8f
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 18:29:04 +0900

    ADD: checkBeforeCreateChatbot middleware

---
+++ src/api/middlewares/checkBeforeCreateChatbot.ts
@@ -0,0 +1,25 @@
+import { Container } from "typedi";
+import { getManager } from "typeorm";
+import { Request, Response, NextFunction } from "express";
+
+export async function checkBeforeCreateChatbot(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { name } = req.body;
+        const entityManager = getManager();
+        const chatbotEntity: Entities.chatbotEntity = Container.get("chatbotEntity");
+        const chatbot = await entityManager.findOne(chatbotEntity, {
+            where: { name }
+        });
+
+        if (chatbot) {
+            return res.status(409).json({
+                message: "This chatbot is aleady exist"
+            });
+        }
+
+        return next();
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+}
