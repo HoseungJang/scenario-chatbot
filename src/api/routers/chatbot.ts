c35d6142df5e4921bda8f81d92b8b2999e2ba3b2
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 16:38:16 +0900

    ADD: POST /:id/skill

--- src/api/routers/chatbot.ts
+++ src/api/routers/chatbot.ts
@@ -2,15 +2,34 @@ import { Router, Request, Response, NextFunction } from "express";
 import { Container } from "typedi";
 import { ChatbotService } from "../../services/chatbot";
 import { IChatbotDTO } from "../../interfaces/IChatbot";
+import { ISkill, ISkillDTO } from "../../interfaces/ISkill";
 import middlewares from "../middlewares";
 
-export default ({ router }: { router: Router }) => {
+const router: Router = Router();
+
+export default ({ app }: { app: Router }) => {
+    app.use("/chatbot", router);
+
     router.post("/", middlewares.checkExistChatbot, async (req: Request, res: Response, next: NextFunction) => {
         try {
             const chatbotServiceInstance = Container.get(ChatbotService);
+            const result = await chatbotServiceInstance.createChatbot(req.body as IChatbotDTO);
+
+            return res.status(201).json(result);
+        } catch (err) {
+            console.error(err);
+            return next(err);
+        }
+    });
+
+    router.post("/:id/skill", async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { name } = req.body;
+            const chatbotId = Number(req.params.id);
+            const chatbotServiceInstance = Container.get(ChatbotService);
             
-            const { id, name, role } = await chatbotServiceInstance.createChatbot(req.body as IChatbotDTO);
-            return res.status(201).json({ id, name, role });
+            const result: ISkill = await chatbotServiceInstance.createSkill({ name, chatbotId } as ISkillDTO);
+            return res.status(201).json(result);
         } catch (err) {
             console.error(err);
             return next(err);
