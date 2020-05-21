ae8d8d6e9e10bf048b1b3f348e0f4b82365594d1
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 18:26:30 +0900

    ADD: checkBeforeCreateSkill middleware

---
+++ src/api/middlewares/checkBeforeCreateSkill.ts
@@ -0,0 +1,36 @@
+import { Container } from "typedi";
+import { getManager } from "typeorm";
+import { Request, Response, NextFunction } from "express";
+
+export async function checkBeforeCreateSkill(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { name } = req.body;
+        const { id: chatbotId } = req.params;
+        const entityManager = getManager();
+        const chatbotEntity: Entities.chatbotEntity = Container.get("chatbotEntity");
+        const skillEntity: Entities.skillEntity = Container.get("skillEntity");
+        
+        const chatbot = await entityManager.findOne(chatbotEntity, chatbotId);
+
+        if (!chatbot) {
+            return res.status(404).json({
+                message: "This chatbot is not exist"
+            });
+        }
+
+        const skill = await entityManager.findOne(skillEntity, {
+            where: { name, chatbotId }
+        });
+
+        if (skill) {
+            return res.status(409).json({
+                message: "This skill is aleady exist"
+            });
+        }
+
+        return next();
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+}
