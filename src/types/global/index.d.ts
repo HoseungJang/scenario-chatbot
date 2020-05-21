bf73dc56b296ecb587715eb74260473f3af82b1c
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 23:24:58 +0900

    ADD: messageEntity type

--- src/types/global/index.d.ts
+++ src/types/global/index.d.ts
@@ -1,11 +1,13 @@
 import { Chatbot } from "../../entities/chatbot";
 import { Skill } from "../../entities/skill";
 import { Block } from "../../entities/block";
+import { Message } from "../../entities/message";
 
 declare global {
     namespace Entities {
         export type chatbotEntity = typeof Chatbot;
         export type skillEntity = typeof Skill;
         export type blockEntity = typeof Block;
+        export type messageEntity = typeof Message;
     }
 }
