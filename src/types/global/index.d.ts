fc6b057dc3a746051a613636acfbc5716e42ddaa
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 16:35:23 +0900

    ADD: skillEntity type

--- src/types/global/index.d.ts
+++ src/types/global/index.d.ts
@@ -1,7 +1,9 @@
 import { Chatbot } from "../../entities/chatbot";
+import { Skill } from "../../entities/skill";
 
 declare global {
     namespace Entities {
         export type chatbotEntity = typeof Chatbot;
+        export type skillEntity = typeof Skill;
     }
 }
