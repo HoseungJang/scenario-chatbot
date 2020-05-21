ee8cf5b5d68154768db0839f1d8e061e75645872
HoseungJang <hsjang.dev@gmail.com>
2020-05-21 19:29:28 +0900

    ADD: blockEntity type

--- src/types/global/index.d.ts
+++ src/types/global/index.d.ts
@@ -1,9 +1,11 @@
 import { Chatbot } from "../../entities/chatbot";
 import { Skill } from "../../entities/skill";
+import { Block } from "../../entities/block";
 
 declare global {
     namespace Entities {
         export type chatbotEntity = typeof Chatbot;
         export type skillEntity = typeof Skill;
+        export type blockEntity = typeof Block;
     }
 }
