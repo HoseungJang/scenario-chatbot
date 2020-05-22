cb73c52a76ff084bb6f61621bfdacb8bb4d1ac15
HoseungJang <hsjang.dev@gmail.com>
2020-05-23 01:40:43 +0900

    ADD: checkBeforeCreateInputBlock middleware

---
+++ src/api/middlewares/checkBeforeCreateInputBlock.ts
@@ -0,0 +1,34 @@
+import { Container } from "typedi";
+import { getManager } from "typeorm";
+import { Request, Response, NextFunction } from "express";
+
+export async function checkBeforeCreateInputBlock(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { previous } = req.body;
+        const entityManager = getManager();
+        const inputEntity: Entities.inputEntity = Container.get("inputEntity");
+        const buttonEntity: Entities.buttonEntity = Container.get("buttonEntity");
+        
+        const input = await entityManager.findOne(inputEntity, previous);
+
+        if (input) {
+            return res.status(409).json({
+                message: "This block already has input block"
+            });
+        }
+
+        
+        const button = await entityManager.findOne(buttonEntity, previous);
+
+        if (button) {
+            return res.status(409).json({
+                message: "This block has button block"
+            });
+        }
+
+        return next();
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+}
--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -1,9 +1,11 @@
 import { checkBeforeCreateChatbot } from "./checkBeforeCreateChatbot";
 import { checkBeforeCreateSkill } from "./checkBeforeCreateSkill";
 import { checkBeforeCreateBlock } from "./checkBeforeCreateBlock";
+import { checkBeforeCreateInputBlock } from "./checkBeforeCreateInputBlock";
 
 export default {
     checkBeforeCreateChatbot,
     checkBeforeCreateSkill,
     checkBeforeCreateBlock,
-};
+    checkBeforeCreateInputBlock,
+}
