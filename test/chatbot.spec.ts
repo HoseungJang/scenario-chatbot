7247ca5db823f78c2ee6bc36156c78e73150e64b
HoseungJang <hsjang.dev@gmail.com>
2020-05-22 11:48:25 +0900

    ADD: createSkill test

--- test/chatbot.spec.ts
+++ test/chatbot.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { Container } from "typedi";
 import { useContainer, createConnection, getManager, getConnection } from "typeorm";
 import { IChatbot, IChatbotDTO } from "../src/interfaces/IChatbot";
+import { ISkill, ISkillDTO } from "../src/interfaces/ISkill";
 import { ChatbotService } from "../src/services/chatbot";
 import { Chatbot } from "../src/entities/chatbot";
 import { Skill } from "../src/entities/skill";
@@ -33,5 +34,20 @@ describe("ChatbotService", async () => {
         const chatbot = await entityManager.findOne(Chatbot, id);
 
         await entityManager.remove(chatbot);
+    });
+
+    it("createSkill", async () => {
+        const entityManager = getManager();
+        const chatbotServiceInstance = Container.get(ChatbotService);
+        const name = "테스트스킬";
+        const chatbotId = -1;
+        const result: ISkill = await chatbotServiceInstance.createSkill({ name, chatbotId } as ISkillDTO);
+
+        expect(result).to.have.all.keys("id", "name");
+
+        const { id } = result;
+        const skill = await entityManager.findOne(Skill, id);
+
+        await entityManager.remove(skill);
     });
 })
