d1b2fd78281cae195f5f212f01c7b3a4d186b4bf
HoseungJang <hsjang.dev@gmail.com>
2020-05-24 11:19:07 +0900

    ADD: block validators

--- src/api/middlewares/checkBeforeCreateButtonBlock.ts
+++ src/api/middlewares/checkBeforeCreateButtonBlock.ts
@@ -4,29 +4,52 @@ import { Request, Response, NextFunction } from "express";
 
 export async function checkBeforeCreateButtonBlock(req: Request, res: Response, next: NextFunction) {
     try {
-        const { previous } = req.body;
+        const { buttons, blockId } = req.body;
         const entityManager = getManager();
+        const blockEntity: Entities.blockEntity = Container.get("blockEntity");
         const buttonEntity: Entities.buttonEntity = Container.get("buttonEntity");
         const inputEntity: Entities.inputEntity = Container.get("inputEntity");
+
+        const previousBlock = await entityManager.findOne(blockEntity, {
+            where: { id: blockId }
+        });
+
+        if (!previousBlock) {
+            return res.status(404).json({
+                message: "Previous block is not exist"
+            });
+        }
         
         const button = await entityManager.findOne(buttonEntity, {
-            where: { previous }
+            where: { previous: blockId }
         });
 
         if (button) {
             return res.status(409).json({
-                message: "This block already has button block"
+                message: "This block already has button blocks"
             });
         }
 
         const input = await entityManager.findOne(inputEntity, {
-            where: { previous }
+            where: { previous: blockId }
         });
 
         if (input) {
             return res.status(409).json({
                 message: "This block has input block"
             });
+        }
+
+        for (const { jumpTo } of buttons) {
+            const jumpToBlock = await entityManager.findOne(blockEntity, {
+                where: { id: jumpTo }
+            });
+    
+            if (!jumpToBlock) {
+                return res.status(404).json({
+                    message: "jumpTo block is not exist"
+                });
+            }
         }
 
         return next();
--- src/api/middlewares/checkBeforeCreateInputBlock.ts
+++ src/api/middlewares/checkBeforeCreateInputBlock.ts
@@ -4,11 +4,32 @@ import { Request, Response, NextFunction } from "express";
 
 export async function checkBeforeCreateInputBlock(req: Request, res: Response, next: NextFunction) {
     try {
-        const { previous } = req.body;
+        const { previous, jumpTo } = req.body;
         const entityManager = getManager();
+        const blockEntity: Entities.blockEntity = Container.get("blockEntity");
         const inputEntity: Entities.inputEntity = Container.get("inputEntity");
         const buttonEntity: Entities.buttonEntity = Container.get("buttonEntity");
         
+        const previousBlock = await entityManager.findOne(blockEntity, {
+            where: { id: previous }
+        });
+
+        if (!previousBlock) {
+            return res.status(404).json({
+                message: "Previous block is not exist"
+            });
+        }
+
+        const jumpToBlock = await entityManager.findOne(blockEntity, {
+            where: { id: jumpTo }
+        });
+
+        if (!jumpToBlock) {
+            return res.status(404).json({
+                message: "jumpTo block is not exist"
+            });
+        }
+
         const input = await entityManager.findOne(inputEntity, {
             where: { previous }
         });
@@ -26,7 +47,7 @@ export async function checkBeforeCreateInputBlock(req: Request, res: Response, n
 
         if (button) {
             return res.status(409).json({
-                message: "This block has button block"
+                message: "This block has button blocks"
             });
         }
 
