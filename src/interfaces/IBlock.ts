bcb06a95ec8da2bfaa8b3a3dae1b298b86917f9f
HoseungJang <hsjang.dev@gmail.com>
2020-05-24 11:21:21 +0900

    ADD: IBlock

--- src/interfaces/IBlock.ts
+++ src/interfaces/IBlock.ts
@@ -7,3 +7,23 @@ export interface IBlockDTO {
     name: string;
     skillId: number;
 }
+
+export interface IBlockInfo {
+    messages: {
+        type: "text" | "image";
+        data: string;
+        slot: boolean;
+    }[];
+    next: {
+        type: "input" | "button";
+        data: {
+            leftText: string;
+            rightText: string;
+            variableName: string;
+            jumpTo: number;
+        } | {
+            data: string;
+            jumpTo: number;
+        }[];
+    }
+}
