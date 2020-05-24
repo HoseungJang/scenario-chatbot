94dec7c9cb4e70a65450f0a29471957d43a3288a
HoseungJang <hsjang.dev@gmail.com>
2020-05-24 14:17:37 +0900

    ADD: checkBeforeGetBlockInfo middleware

---
+++ src/api/middlewares/checkBeforeGetBlockInfo.ts
@@ -0,0 +1,26 @@
+import { Container } from "typedi";
+import { getManager } from "typeorm";
+import { Request, Response, NextFunction } from "express";
+
+export async function checkBeforeGetBlockInfo(req: Request, res: Response, next: NextFunction) {
+    try {
+        const id = Number(req.params.id);
+        const entityManager = getManager();
+        const blockEntity: Entities.blockEntity = Container.get("blockEntity");
+        
+        const block = await entityManager.findOne(blockEntity, {
+            where: { id }
+        });
+
+        if (!block) {
+            return res.status(404).json({
+                message: "This block is not exist"
+            });
+        }
+
+        return next();
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+}
